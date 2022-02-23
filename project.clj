3e7d8832556539be38c5adf7d0878e271f2041e1
Esko Suomi <esko.suomi@solita.fi>
2022-02-23 18:42:47 +0200

    Override Clojars configuration to avoid GPG
    GPG is terrible to use, there's not much else to it.

--- project.clj
+++ project.clj
@@ -3,9 +3,15 @@
   :url "https://github.com/esuomi/lein-git-revisions"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+
   :eval-in-leiningen true
 
   :dependencies [[me.arrdem/cuddlefish "0.1.0"]]
+
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url           "https://clojars.org/repo"
+                                    :username      :env/CLOJARS_USERNAME
+                                    :password      :env/CLOJARS_TOKEN}]]
 
   ;:plugins [[fi.polycode/lein-git-revisions "0.0.0"]]
 
