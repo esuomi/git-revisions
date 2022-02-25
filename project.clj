552bd100e72591e4663a5a0b46c5cddf70b2d68c
Esko Suomi <esko.suomi@solita.fi>
2022-02-25 19:21:29 +0200

    add :scm to POM to enable cljdocs rendering

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
+  :scm {:name "git" :url "https://github.com/esuomi/lein-git-revisions"}
+
   :eval-in-leiningen true
 
   :dependencies [[me.arrdem/cuddlefish "0.1.0"]]
