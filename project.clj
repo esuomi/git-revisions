203966f3902bb37e776c8b580778fe5345c7eef5
Esko Suomi <esko.suomi@solita.fi>
2022-06-08 08:00:27 +0300

    remove Leiningen plugin code from repository

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :scm {:name "git" :url "https://github.com/esuomi/lein-git-revisions"}
+  :scm {:name "git" :url "https://github.com/esuomi/git-revisions-lein"}
 
   :eval-in-leiningen true
 
